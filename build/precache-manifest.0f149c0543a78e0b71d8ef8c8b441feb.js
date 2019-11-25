81c42003fa74bb19a733dfb1c81853a5ca331499
rob3rtroessler <robertroessler@g.harvard.edu>
2019-11-25 11:51:35 -0500

    init

---
+++ build/precache-manifest.0f149c0543a78e0b71d8ef8c8b441feb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765bbfe02c2b522609ec15d61b6f8004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1f5e82654debc63004c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1b26744e337d9103fc42",
+    "url": "/static/js/2.1c2ff428.chunk.js"
+  },
+  {
+    "revision": "f1f5e82654debc63004c",
+    "url": "/static/js/main.f3f333a5.chunk.js"
+  },
+  {
+    "revision": "c19d729c9a1acbbc0b3b",
+    "url": "/static/js/runtime-main.74540a05.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
